328d525705a7b7392311980e4309a966942cf6bf
kthxxx <152075319+kthxxx@users.noreply.github.com>
2024-03-28 00:53:15 +0800

    Create ReassigningValuesToVariables.java

---
+++ Chapter 9/ReassigningValuesToVariables.java
@@ -0,0 +1,17 @@
+/*  This program prints the quadratic value of x.
+    Name: Emeterio, Keith Justin O.
+    Date: Feb 08, 2024
+*/
+
+public class ReassigningValuesToVariables
+{
+    public static void main (String[] args)
+    {
+        double x = 2.0;
+        double value;
+
+        value = (3 * (x * x)) - (8 * x) + 4;
+
+        System.out.println("At x =  " + x + " the value is " + value);
+    }
+}
